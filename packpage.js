21d155c8aaecea8580b83c7cd31145a8cd99b4f3
BARBAROSSA <enesademi73@gmail.com>
2026-04-20 03:04:29 +0200

    Create packpage.js

---
+++ packpage.js
@@ -0,0 +1,18 @@
+{
+  "name": "follower-app",
+  "version": "1.0.0",
+  "main": "src/index.js",
+  "dependencies": {
+    "axios": "^0.21.1",
+    "react": "^17.0.2",
+    "react-dom": "^17.0.2",
+    "react-scripts": "5.0.0",
+    "styled-components": "^5.3.5"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+  }
+}
